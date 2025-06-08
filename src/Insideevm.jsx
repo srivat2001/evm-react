9a3a8e160896d204f91055b8688f1de2062b019e
Srivathsan G <77399329+srivat2001@users.noreply.github.com>
2025-06-08 10:00:10 +0530

    moved testing from dep to devdep and changed class to arrow functions

--- src/Insideevm.jsx
+++ src/Insideevm.jsx
@@ -1,48 +1,36 @@
-import React, { Component} from 'react';
+import  { useState } from 'react';
 import './evm.css';
-import Votevalue from "./Partyholder";
-class Insideevm1 extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
+import Votevalue from './Partyholder';
 
-      disabled :false
-    };
-  
-      }
-    
-    partylist= 
-        {
-            party:[
+const Insideevm1 = ({ updated }) => {
+  const [disabled, setDisabled] = useState(false);
 
-              {
-                name:"BJP",votes:0,link:require('./img/Bjp.png')
-              },
-              {
-                name:"Congress",votes:0,link:require('./img/INC_Logo.png')
-              }
-            ]
-   
-        };
-        cooldownfc = (obj1) =>{
-          this.setState({disabled: true})
-          setTimeout(() => this.setState({ disabled: false }), 0);
-          this.props.updated(obj1);
-   }
+  const partylist = [
+    { name: 'BJP', votes: 0, link: require('./img/Bjp.png') },
+    { name: 'Congress', votes: 0, link: require('./img/INC_Logo.png') }
+  ];
 
-    render() {
-      return (
-           <div className='voterboxinside'> {this.partylist.party.map(party => 
-            <Votevalue votebtn={this.state.disabled}cooldownfc={this.cooldownfc} imglink={party.link} partyname={party.name} vote={party.votes} selected={true} ></Votevalue>
-        
-          )
-          
-          
-          } </div>);
-        
-       
-        
-    }
-}
-  
-  export default Insideevm1
+  const cooldownfc = (obj1) => {
+    setDisabled(true);
+    setTimeout(() => setDisabled(false), 2000); // 2-second cooldown
+    updated(obj1);
+  };
+
+  return (
+    <div className='voterboxinside'>
+      {partylist.map((party) => (
+        <Votevalue
+          key={party.name}
+          votebtn={disabled}
+          cooldownfc={cooldownfc}
+          imglink={party.link}
+          partyname={party.name}
+          vote={party.votes}
+          selected={true}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Insideevm1;
--- src/Pritham.jsx
+++ src/Pritham.jsx
@@ -1,29 +1,23 @@
-import React,{Component} from 'react';
+import React, { useState } from 'react';
 import './evm.css';
 import Insideevm1 from "./Insideevm";
-import Vvpat from "./Vvpat"
-  class Car extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        partyselected :"NULL"
-      }
+import Vvpat from "./Vvpat";
 
-    }
-    updatevvpat = (party) =>
-    {
-     
-      this.setState({partyselected :party} )
-  
- }
-    render() {
-      return <div className='MainBox'>
-        <Vvpat partyvoted={this.state.partyselected}/>
-        <div className='outerbox'>
-            <Insideevm1 updated={this.updatevvpat}/>
-          </div>
-        </div>;
-    }
-  }
-  
-  export default Car
+const Car = () => {
+  const [partySelected, setPartySelected] = useState("NULL");
+
+  const updateVvpat = (party) => {
+    setPartySelected(party);
+  };
+
+  return (
+    <div className='MainBox'>
+      <Vvpat partyvoted={partySelected} />
+      <div className='outerbox'>
+        <Insideevm1 updated={updateVvpat} />
+      </div>
+    </div>
+  );
+};
+
+export default Car;
